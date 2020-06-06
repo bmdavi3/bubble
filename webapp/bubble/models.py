45f93ef15948b9a6bfd65590effa7c8ea69af221
Brian Davis <the.brian.m.davis@gmail.com>
2020-06-06 17:49:02 -0500

    Add models and migrations

--- webapp/bubble/models.py
+++ webapp/bubble/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Jug(models.Model):
+    name = models.TextField()
+
+    class Meta:
+        db_table = 'jug'
+
+
+class Bubble(models.Model):
+    jug = models.ForeignKey(Jug, on_delete=models.CASCADE)
+    created = models.DateTimeField()  # A DB level default of now() is added in a manual migration
+
+    class Meta:
+        db_table = 'bubble'
+
